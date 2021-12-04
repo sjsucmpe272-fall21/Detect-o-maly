62727b720dd00235d0f20b5a3caeb81df07bfbce
yashwanth6 <yashu654321@gmail.com>
2021-12-04 12:06:18 -0800

    package json changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { frontendIP, frontendPort } from "./serverConfig.js";
+const session = require('express-session');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(
 app.use(express.json());
 app.use(
 	express.urlencoded({
+
 		extended: false,
 	})
 );
